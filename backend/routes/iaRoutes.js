0705803116afab48ca2f2a0bc6f950410439aa94
Joshua4167 <70658240+Joshua4167@users.noreply.github.com>
2026-02-05 18:28:27 -0500

    Delete backend directory

--- backend/routes/iaRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const iaController = require('../controllers/iaController');
-
-router.post('/analizar-fisio', iaController.analizarFisio);
-
-module.exports = router;
